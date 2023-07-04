adb202a2a44bb3eef4a31a4a306cbb5c2a69af56
Douglas Amorim <douglas_amorimm@outlook.com>
2023-07-04 11:18:18 -0300

    estrtura do config

--- 03sqla_sync/conf/db_session.py
+++ 03sqla_sync/conf/db_session.py
@@ -26,7 +26,10 @@ load_dotenv()
 # definir qual vai ser a connection com qual o banco
 __engine: Optional[Engine] = None
 
-def create_engine(sqlite:bool = False):
+def create_engine(sqlite:bool = False) -> Engine:
+    """
+        Função para configurar a conexão ao banco de dados
+    """
     global __engine
     
     if __engine:
@@ -42,4 +45,34 @@ def create_engine(sqlite:bool = False):
         __engine = sa.create_engine(url=conn, echo=False, connect_args={'check_same_thread' : False})
         
     else:
-        conn = f'postgres://{getenv("PGSQL_USERNAME")}:{getenv("PGSQL_PASSWORD")}@localhost:5432/picoles'
+        conn = f'postgresql://{getenv("PGSQL_USERNAME")}:{getenv("PGSQL_PASSWORD")}@localhost:5432/picoles'
+        __engine = sa.create_engine(url=conn, echo=False)
+
+    return __engine
+
+def create_session() -> Session:
+    """
+        Função para criar a sessão de conexão ao banco de dados
+    """
+    global __engine
+    
+    if not __engine:
+        create_engine()  # Se não usar o Postgres => create_engine(sqlite=True)
+    
+    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
+    
+    session: Session = __session()
+    
+    return session
+
+def create_tables() -> None:
+    global __engine
+    
+    if not __engine:
+        create_engine()
+    
+    import models.__all_models
+    # Vai apagar todas a tabelas
+    ModelBase.metadata.drop_all(__engine)
+    #Vai criar todas as tabelas
+    ModelBase.metadata.create_all(__engine)
